5cc2a08fe815e75713d1d37c6fe992c236519d9d
mehrajcse1 <mehrajcse1@gmail.com>
2022-12-03 19:31:31 +0530

    adding program infro

--- rt_downward_triangle.py
+++ rt_downward_triangle.py
@@ -1,6 +1,6 @@
 '''Right Triangle Star Pattern'''
-N=5
-for i in range(N):
-    for j in range(i,N):
+N=5 #Nuumber of rows
+for i in range(N): #loop for rows
+    for j in range(i,N):    #loop that runs inside a row
         print("*", end=" ")
     print()
--- rt_triange_pattern.py
+++ rt_triange_pattern.py
@@ -1,6 +1,6 @@
 '''Right Triangle Star Pattern'''
-N=5
-for i in range(N):
-    for j in range(i+1):
+N=5 #Number of rows we want
+for i in range(N):  #loop for rows
+    for j in range(i+1): #loop to run Inside a row 
         print("*", end=" ")
     print()
